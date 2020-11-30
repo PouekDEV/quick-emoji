467e80dbf52e1ebdb207c3782bd325ec51a5e023
Jakub Starostecki <64737924+PouekDEV@users.noreply.github.com>
2020-11-30 10:34:29 +0100

    files

---
+++ main.js
@@ -0,0 +1,168 @@
+/*!
+ *   quick-emoji <https://github.com/PouekDEV/quick-emoji>
+ */
+module.exports = function qemoji(emoji){
+    switch(emoji){
+        case "smile":
+            emoji += "😀";
+            emoji = emoji.replace(/smile/g,"");
+        break;
+        case "smiley":
+            emoji += "😃";
+            emoji = emoji.replace(/smiley/g,"");
+        break;
+        case "grin":
+            emoji += "😁";
+            emoji = emoji.replace(/grin/g,"");
+        break;
+        case "XD":
+            emoji += "😆";
+            emoji = emoji.replace(/XD/g,"");
+        break;
+        case "joy":
+            emoji += "😂";
+            emoji = emoji.replace(/joy/g,"");
+        break;
+        case "upsided":
+            emoji += "🙃";
+            emoji = emoji.replace(/upsided/g,"");
+        break;
+        case "sweat":
+            emoji += "😅";
+            emoji = emoji.replace(/sweat/g,"");
+        break;
+        case "wink":
+            emoji += "😉";
+            emoji = emoji.replace(/wink/g,"");
+        break;
+        case "halo":
+            emoji += "😇";
+            emoji = emoji.replace(/halo/g,"");
+        break;
+        case "loudcrying":
+            emoji += "😭";
+            emoji = emoji.replace(/loudcrying/g,"");
+        break;
+        case "pleading":
+            emoji += "🥺";
+            emoji = emoji.replace(/pleading/g,"");
+        break;
+        case "heart":
+            emoji += "❤️";
+            emoji = emoji.replace(/heart/g,"");
+        break;
+        case "sparkles":
+            emoji += "✨";
+            emoji = emoji.replace(/sparkles/g,"");
+        break;
+        case "smileheart":
+            emoji += "😍";
+            emoji = emoji.replace(/smileheart/g,"");
+        break;
+        case "pray":
+            emoji += "🙏";
+            emoji = emoji.replace(/pray/g,"");
+        break;
+        case "coveredhearts":
+            emoji += "🥰";
+            emoji = emoji.replace(/coveredhearts/g,"");
+        break;
+        case "starface":
+            emoji += "🤩";
+            emoji = emoji.replace(/starface/g,"");
+        break;
+        case "blowingkiss":
+            emoji += "😘";
+            emoji = emoji.replace(/blowingkiss/g,"");
+        break;
+        case "kissing":
+            emoji += "😗";
+            emoji = emoji.replace(/kissing/g,"");
+        break;
+        case "savouring":
+            emoji += "😋";
+            emoji = emoji.replace(/savouring/g,"");
+        break;
+        case "tongueface":
+            emoji += "😛";
+            emoji = emoji.replace(/tongueface/g,"");
+        break;
+        case "zanyface":
+            emoji += "🤪";
+            emoji = emoji.replace(/zanyface/g,"");
+        break;
+        case "XDtongue":
+            emoji += "😝";
+            emoji = emoji.replace(/XDtongue/g,"");
+        break;
+        case "hugface":
+            emoji += "🤗";
+            emoji = emoji.replace(/hugface/g,"");
+        break;
+        case "thinkingface":
+            emoji += "🤔";
+            emoji = emoji.replace(/thinkingface/g,"");
+        break;
+        case "zippermouth":
+            emoji += "🤐";
+            emoji = emoji.replace(/zippermouth/g,"");
+        break;
+        case "neutral":
+            emoji += "😐";
+            emoji = emoji.replace(/neutral/g,"");
+        break;
+        case "raisedeyebrow":
+            emoji += "🤨";
+            emoji = emoji.replace(/raisedeyebrow/g,"");
+        break;
+        case "pokerface":
+            emoji += "😶";
+            emoji = emoji.replace(/pokerface/g,"");
+        break;
+        case "sleepy":
+            emoji += "😴";
+            emoji = emoji.replace(/sleepy/g,"");
+        break;
+        case "mask":
+            emoji += "😷";
+            emoji = emoji.replace(/mask/g,"");
+        break;
+        case "hotface":
+            emoji += "🥵";
+            emoji = emoji.replace(/hotface/g,"");
+        break;
+        case "coldface":
+            emoji += "🥶";
+            emoji = emoji.replace(/coldface/g,"");
+        break;
+        case "yaw":
+            emoji += "🥱";
+            emoji = emoji.replace(/yaw/g,"");
+        break;
+        case "poutingface":
+            emoji += "😡";
+            emoji = emoji.replace(/poutingface/g,"");
+        break;
+        case "angry":
+            emoji += "😠";
+            emoji = emoji.replace(/angry/g,"");
+        break;
+        case "facewithsymbols":
+            emoji += "🤬";
+            emoji = emoji.replace(/facewithsymbols/g,"");
+        break;
+        case "smilinghorns":
+            emoji += "😈";
+            emoji = emoji.replace(/smilinghorns/g,"");
+        break;
+        case "angryhorns":
+            emoji += "👿";
+            emoji = emoji.replace(/angryhorns/g,"");
+        break;
+        case "skull":
+            emoji += "💀";
+            emoji = emoji.replace(/skull/g,"");
+        break;
+    }
+    return emoji;
+}
---
+++ test.js
@@ -0,0 +1,4 @@
+const qemoji = require('./main');
+console.log(qemoji("smile")) //prints smiling face
+console.log(qemoji("heart")) //prints heart
+console.log(qemoji("skull")) //prints skull
